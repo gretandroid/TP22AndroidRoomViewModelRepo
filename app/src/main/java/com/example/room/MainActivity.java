a514633c37140e331295d40d63817d9dc14f070a
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-04-19 10:42:46 +0200

    ajout du menu

--- app/src/main/java/com/example/room/MainActivity.java
+++ app/src/main/java/com/example/room/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.room;
 
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.room.database.PersonneEntity;
+import com.example.room.viewmodel.MainViewModel;
+
 public class MainActivity extends AppCompatActivity {
+    private ListView listView;
+    private ArrayAdapter<PersonneEntity> arrayAdapter;
+    private MainViewModel mViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
